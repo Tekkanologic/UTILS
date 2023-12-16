f032f9c439da2fbc8781a87419548925856444d1
lukevc <lukevincentcannon@hotmail.co.uk>
2023-12-16 12:02:04 +0000

    Update uber_reader.py

--- uber_reader.py
+++ uber_reader.py
@@ -1,5 +1,11 @@
 import PyPDF2
 from decimal import Decimal
+import statistics
+import math
+
+def truncate(n, decimals=2):
+    multiplier = 10**decimals
+    return int(n * multiplier) / multiplier
 
 def search_keyword(pdf_file, keyword):
     array = []
@@ -16,8 +22,18 @@ def search_keyword(pdf_file, keyword):
                     array.append(Decimal(next_word[1:]))
     total = sum(array)
     total_2 = len(array) * 5.25
-    print(f"Total: £{total}, Total contribution: £{total_2}")
+    num_of_journeys = len(array)
+    lowest_journey = min(array)
+    highest_journey = max(array)
+    mean_journeys = truncate(statistics.mean(array))
+    print(f"Number of journeys: {num_of_journeys} \nBetween (£{lowest_journey} ~ £{highest_journey}) \nMean: £{mean_journeys} \nTotal: £{total} \nTotal contribution: £{total_2}\n ")
+    print(f"Total to claim: {Decimal(total) - Decimal(total_2)}")
+    return total, total_2, num_of_journeys, lowest_journey, highest_journey, mean_journeys
+
 
 if __name__ == "__main__":
     pdf_file = r"C:\Users\LukeV\OneDrive\My Documents\Uber reciepts\Uber reciepts\01 - October 2023\merged.pdf"
-    search_keyword(pdf_file, "Payments")
+    with open('output.txt', 'w') as file:
+        total, total_2, num_of_journeys, lowest_journey, highest_journey, mean_journeys = search_keyword(pdf_file, "Payments")
+        file.write(f"Number of journeys: {num_of_journeys} \nBetween (£{lowest_journey} ~ £{highest_journey}) \nMean: £{mean_journeys} \nTotal Cost: £{total} \nTotal Contribution: £{total_2}\n")
+        file.write(f"Total to claim back: {Decimal(total) - Decimal(total_2)}")
